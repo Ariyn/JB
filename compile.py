ba938c22f11f9907a270f4c81efb3eab4a176230
Hwang Min-uk <himnowxz@gmail.com>
2016-05-20 17:55:23 +0900

    add some more syntax

--- compile.py
+++ compile.py
@@ -7,13 +7,23 @@ import re
 ### MIT License
 
 class markdown:
+	# skip escaped bracket with backward lookahead
+	# (\[.+?(?<!\\)\])
+# <a href=\"#\1\">\1</a>
 	mdSyntax = [
+		(r"#{1,6} .+?\n((?:  )?[\*\+-] (.+?)(?=\n\n))", "uiList"),
+		(r"\n{2,}([\*\+-] (.+?)(?=\n\n))", "uiList"),
+		
 		(r"\*\*(.+?)\*\*", r"<strong>\1</strong>"),
 		(r"(---|___|\*\*\*)", r"<hr>\n"),
 		(r"(\*|_)(.+?)(\*|_)", r"<em>\2</em>"),
 		(r"~~(.+?)~~", r"<del>\1</del>"),
-		(r"\n{2,}([\*\+-] (.+?)(?=\n\n))", "uiList"),
-		(r"((<(.+?)>)?.+(<\/\3>)?)",r"<p>\1</p>")
+		(r"(\[(.+?)(?<!\\)\]\((.+?)\))", "anchor"),
+
+		(r"((#{1,6}) (.+?\n))", r"header"),
+		(r"(^(>+)(.+?)(?=\n\n))", "block"),
+
+		(r"\n?((<(.+?)>)?(.+?)(<\/\3>)?\n)","pSign"),
 	]
 	
 	def __init__(self):
@@ -29,22 +39,25 @@ class markdown:
 				if ext in ["md", "MD"]:
 					print(fullPath)
 					f = self.parseMD(fullPath)
-					print(f)
+					# print(f)
 
 	def parseMD(self, path):
-		content = codecs.open(path, "r", "UTF-8").read()
+		content = codecs.open(path, "r", "UTF-8").read()+"\n\n"
 		for (pattern, repl) in self.mdSyntax:
 			if hasattr(self, repl):
-
 				func = self.__getattribute__(repl)
 				# print(func, type(func), dir(func))
+				
 				search = re.findall(pattern, content, flags=re.M|re.S)
 				for find in search:
 					d = func.__call__(find)
-					content = content.replace(find[0], d)
+
+					if d:
+						content = content.replace(find[0], d)
 			else:
 				content = re.sub(pattern, repl, content)
 
+		content = "<head><meta charset=\"utf-8\"></head>\n<body>"+content+"\n</body>"
 		return content
 
 	def uiList(self, find, depth=0):
@@ -84,6 +97,40 @@ class markdown:
 				retVal.append(ret)
 		return "<ul>"+"\n".join(retVal)+"</ul>" if depth == 0 else retVal;
 
+	def pSign(self, find):
+		if find[2] in ["ul", "li", "ol", "h1", "h2", "h3", "h4", "h5", "h6" ,"hr"] or find[2].find("!--") == 0:
+			return None
+
+		match = re.match("\s+", find[0])
+		if match:
+			return find[0]
+
+		retVal = "<p>%s</p>"%find[0]
+		count = retVal.count("\n")
+
+		retVal = retVal.replace("\n", "")+"".join(["\n"]*count);
+		return retVal
+
+	def anchor(self, find):
+		# <a href="#chapter-3">Chapter 3</a>
+		return "<a href=\"%s\">%s</a>" % (find[2], find[1])
+
+	def header(self, find):
+		header = re.search(r"(?:<.+?>)?(.+?)(?:<\/.+?>)?\n", find[2]).groups()[0]
+		count, nlCount = find[1].count("#"), find[2].count("\n")
+
+
+		retVal = ("<h%d id=\"%s\"><a href=\"#%s\">%s</a></h%d>"% (count, header, header, find[2], count)).replace("\n", "")
+		# print("header : ",header, "retVal : ", retVal)
+
+		retVal = retVal+"".join(["\n"]*nlCount);
+		return retVal
+
+	def block(self, find, oriDepth = 1):
+		# find = find[0]
+		# newFind = re.findall("(>{%d}(.+))\s?"%oriDepth, find)
+		# print(newFind)
+		pass
 
 if __name__ == "__main__":
 	md = markdown()
