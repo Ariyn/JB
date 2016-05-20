028cfbc7288d61859babd02fcda37a36cbcdc317
Hwang Min-uk <himnowxz@gmail.com>
2016-05-21 04:56:42 +0900

    syntax done

--- compile.py
+++ compile.py
@@ -6,10 +6,29 @@ import re
 ### inspired by https://gist.github.com/jbroadway/2836900
 ### MIT License
 
+# skip escaped bracket with backward lookahead
+# (\[.+?(?<!\\)\])
+
+# TODO
+
+# dynamically class name and id
+# to make css custom module possible
+
+# publish target folder
+
+# hide html option
+
+# parse site.json
+
+# image
+
+# addon and code embeded template
+	# recently updated
+
+# ajax comment system 
+
+
 class markdown:
-	# skip escaped bracket with backward lookahead
-	# (\[.+?(?<!\\)\])
-# <a href=\"#\1\">\1</a>
 	mdSyntax = [
 		(r"#{1,6} .+?\n((?:  )?[\*\+-] (.+?)(?=\n\n))", "uiList"),
 		(r"\n{2,}([\*\+-] (.+?)(?=\n\n))", "uiList"),
@@ -27,19 +46,35 @@ class markdown:
 	]
 	
 	def __init__(self):
-		# print(dir(self))
-		pass
+		self.mdLists = {}
 
-	def searchFolder(self, path):
-		for (path, dir, files) in os.walk(path):
+	def searchFolder(self, rootPath):
+		rootPath = rootPath.replace("\/", "/").replace("\\", "/")+"/"
+		# print(rootPath)
+		for (path, dir, files) in os.walk(rootPath):
+			path = path.replace("\/", "/").replace("\\", "/")
+			relPath = path.replace(rootPath, "/")
+
+			if relPath not in self.mdLists:
+				self.mdLists[relPath] = []
+
 			for file in files:
 				fullPath = path+"/"+file
 				ext = file.split(".")[-1]
 
 				if ext in ["md", "MD"]:
-					print(fullPath)
-					f = self.parseMD(fullPath)
-					# print(f)
+					print(fullPath, relPath)
+					self.mdLists[relPath].append({
+						"path":relPath+"/"+file,
+						"fullPath":fullPath,
+						"ext":ext,
+					})
+		for i in self.mdLists:
+			folders = self.mdLists[i]
+
+			for md in folders:
+				f = self.parseMD(md["fullPath"])
+				codecs.open(md["fullPath"].replace(md["ext"], "html"), "w", "utf-8").write(f)
 
 	def parseMD(self, path):
 		content = codecs.open(path, "r", "UTF-8").read()+"\n\n"
@@ -113,24 +148,84 @@ class markdown:
 
 	def anchor(self, find):
 		# <a href="#chapter-3">Chapter 3</a>
-		return "<a href=\"%s\">%s</a>" % (find[2], find[1])
+		targetPath = find[2]
+
+		if targetPath[0] == '$':
+			# internal page link
+			targetPath = targetPath[1:]
+			path = "/contents/"+"/".join(targetPath.split("/")[:-1])
+
+			# print(path, targetPath, self.mdLists)
+			if path in self.mdLists:
+				# print(path)
+				# print(self.mdLists[path])
+
+				for i in self.mdLists[path]:
+					# print(targetPath)
+					if i["path"] == "/contents/"+targetPath+".md":
+						targetPath = "./"+targetPath+".html"
+						break
+			
+		elif targetPath.find('#') == 0:
+			# html tag link
+			pass
+		else:
+			# external link
+			pass
+		return "<a href=\"%s\">%s</a>" % (targetPath, find[1])
 
 	def header(self, find):
 		header = re.search(r"(?:<.+?>)?(.+?)(?:<\/.+?>)?\n", find[2]).groups()[0]
 		count, nlCount = find[1].count("#"), find[2].count("\n")
 
 
-		retVal = ("<h%d id=\"%s\"><a href=\"#%s\">%s</a></h%d>"% (count, header, header, find[2], count)).replace("\n", "")
+		retVal = ("<h%d id=\"%s\"><a href=\"#%s\">%s</a></h%d>"% (count, header, "Table of Contents", find[2], count)).replace("\n", "")
 		# print("header : ",header, "retVal : ", retVal)
 
 		retVal = retVal+"".join(["\n"]*nlCount);
 		return retVal
 
-	def block(self, find, oriDepth = 1):
-		# find = find[0]
-		# newFind = re.findall("(>{%d}(.+))\s?"%oriDepth, find)
+	def block(self, find, depth = 1):
+		find = find[0]
+		htmlList = []
+		newFind = re.findall("((>*)(.+))\s?", find)
 		# print(newFind)
-		pass
+
+		skip, newList = False, []
+		newDepth = depth
+		# TODO:
+		# to much nested if and for loop
+
+		for i, v in enumerate(newFind):
+			
+			# if v[2] == "sample":
+			# 	print(v, newDepth, depth)
+			if skip:
+				newCount = v[1].count(">")
+
+				if 0 != newCount < newDepth:
+					# print("block newList", newList)
+					d = self.block(["\n".join(newList)], newDepth)
+					# print("d", d)
+					htmlList.append(d)
+					skip, newList, newDepth = False, [], depth
+					newText = re.sub(">{%d}(.+)"%newDepth, r"\1", v[0])
+					htmlList.append(newText)
+				else:
+					newList.append(v[0])
+			elif depth < v[1].count(">"):
+				# print("inside!")
+				skip, newDepth = True, v[1].count(">")
+
+				# print(newList)
+				newList.append(v[0])
+			else:
+				newText = re.sub(">{%d}(.+)"%newDepth, r"\1", v[0])
+				htmlList.append(newText)
+
+		# print(htmlList)
+
+		return "<blockquote><p>"+"<br />".join(htmlList)+"</p></blockquote>"
 
 if __name__ == "__main__":
 	md = markdown()
