e088033f855fa8d948b852d1c5ba8db5c7a1161f
Ariyn <himnowxz@gmail.com>
2018-12-08 14:24:33 +0900

    trevis customization

--- test/test_compiler.py
+++ test/test_compiler.py
@@ -9,20 +9,20 @@ sampleConfig = {
 	"path":{
 		"build":"build",
 		"contents":"contents",
-		"resource":"resources"
+		"resource":"resources",
+		"skin":"skins/simple-blog-theme"
 	},
 	"site":{
 		"author":{
-			"nickname":"ariyn",
-			"name":"MinUk Hwang",
-			"email":"himnowxz@gmail.com"
+			"nickname":"T",
+			"name":"Testio Testo",
+			"email":"Test@mail.example.org"
 		},
-		"domainName":"this.ismin.uk",
-		"name":"this is\nmin-uk",
-		"description":"this is min-uk's personal web site.",
+		"domainName":"example.org",
+		"name":"sample web site",
+		"description":"this is sample web site.",
 		"prefix":"JB-build"
-	},
-	"skin":"/home/wssh/JB-Blog/skins/clean blog gh pages/"
+	}
 }
 
 # print =None
@@ -30,7 +30,7 @@ class CompileTester(unittest.TestCase):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.rootPath = "/tmp/JB-Blog/"
-		
+
 		if not os.path.exists(self.rootPath):
 			os.mkdir(self.rootPath)
 		for path in [os.path.join(self.rootPath,i) for i in sampleConfig["path"].keys()]:
@@ -40,12 +40,12 @@ class CompileTester(unittest.TestCase):
 				pass
 		with open(os.path.join(self.rootPath, "config.json"), "w") as f:
 			f.write(json.dumps(sampleConfig))
-			
-		
+
+
 	def test_init(self):
 		c = Compiler(self.rootPath)
 		c.compile()
-		
+
 	def test_path(self):
 		c = Compiler(self.rootPath)
 		self.assertEqual(c.build, "build")
--- test/test_skin.py
+++ test/test_skin.py
@@ -8,19 +8,19 @@ from mako.lookup import TemplateLookup
 
 from src.skin import SkinManager
 
-rootPath = "/home/wssh/JB/"
-path = "/home/wssh/JB-Blog/skins"
+rootPath = "/tmp/JB/"
+path = "/tmp/JB-Blog/skins"
 articleKeys = ["content", "path", "birthDate", "birthDateStr", "name", "title", "heading", "bgImage"]
 newStyleArticle = {
 	"site":{
 		"tagline":"tagline sample text",
 		"author" : {
-			"nickname":"ariyn",
-			"name": "Min-Uk Hwang",
-			"email":"himnowxz@gmail.com"
+			"nickname":"T",
+			"name": "Testio Testo",
+			"email":"Test@mail.example.org"
 		},
-		"domainName":"this.ismin.uk",
-		"name":"this.ismin.uk",
+		"domainName":"example.org",
+		"name":"sample web site",
 		"description":"this is sample page for JB."
 	},
 	"post":{
@@ -32,16 +32,16 @@ newStyleArticle = {
 
 class SkinTester(unittest.TestCase):
 	def test_importSkin(self):
-		s = SkinManager(path, "clean blog gh pages")
+		s = SkinManager(path, "simple-blog-theme")
 		skinData = s.get("article")
-		
+
 	def test_compile(self):
 		article = {k:k for k in articleKeys}
-		
-		s = SkinManager(path, "clean blog gh pages")
+
+		s = SkinManager(path, "simple-blog-theme")
 		skin = s.compile("article", newStyleArticle)
 # 		print(skin.file)
-	
+
 	def test_makoTemplate(self):
 # 		mylookup = TemplateLookup(directories=['/home/wssh/JB/samples/'])
 		mytemplate = Template(filename="samples/template.html")
